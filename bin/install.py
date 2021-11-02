5ea5ac6024e0ca14f72bf0bc133ebe9ce3fe742c
Danial Manavi <dmanavi@live.com>
2021-11-01 22:14:42 -0700

    Update python script.

--- bin/install.py
+++ bin/install.py
@@ -2,12 +2,12 @@
 
 import os
 import platform
-import requests
+from urllib import request
 from zipfile import ZipFile
 import psutil
 import re as regex
 from io import BytesIO
-from stat import S_IEXEC
+from json import load as load_json
 
 # get OS related information
 os_name = None
@@ -43,23 +43,24 @@ if cpu_architecture is None:
 
 # get latest release version
 print(f"Finding the latest version...")
-release_api_result = requests.get("https://api.github.com/repos/getignore/getignore/releases/latest")
-if release_api_result.ok is False:
+release_api_response = request.urlopen(url="https://api.github.com/repos/getignore/getignore/releases/latest")
+
+if release_api_response.status < 200 or release_api_response.status > 299:
     print(f"Failed to communicate with GitHub release API.")
-    print(release_api_result.text)
+    print(release_api_response.read())
     exit(2)
 
-release_api_result_json = release_api_result.json()
+release_api_result_json = load_json(release_api_response)
 latest_version = release_api_result_json['tag_name']
 
 # download os-specific artifact file
 print(f"Downloading artifact '{latest_version}' for {os_name}_{cpu_architecture} ...")
 artifact_url = f"https://github.com/GetIgnore/getignore/releases/download/{latest_version}/getignore-{os_name}_{cpu_architecture}.zip"
-artifact_response = requests.get(url=artifact_url)
+artifact_api_response = request.urlopen(url=artifact_url)
 
-if artifact_response.ok is False:
+if artifact_api_response.status < 200 or artifact_api_response.status > 299:
     print(f"Failed to download artifact from '{artifact_url}'")
-    print(artifact_response.text)
+    print(artifact_api_response.read())
     exit(2)
 
 # extract artifact to the OS-specific bin directory
@@ -71,7 +72,7 @@ os_bin_dir = dict({
 extract_target_path = os_bin_dir.get(os_name)
 
 print(f"Extracting artifact to '{extract_target_path}' ...")
-with ZipFile(BytesIO(artifact_response.content), mode='r') as zipFile:
+with ZipFile(BytesIO(artifact_api_response.read()), mode='r') as zipFile:
     zipFile.extractall(path=extract_target_path)
     
     # make the code executable
